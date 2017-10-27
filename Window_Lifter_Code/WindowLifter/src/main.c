13240b044e66fad129c4e99e11ab3ae6838b7cd6
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-10-27 11:05:59 -0500

    Code_Version_3
    Adding a variable "level" to define the position of the window (number of the LED).
    Creating a two LED routine by pressing the pushbutton defined as "PUSHUP"

--- Window_Lifter_Code/WindowLifter/src/main.c
+++ Window_Lifter_Code/WindowLifter/src/main.c
@@ -5,6 +5,8 @@
 
 
 int  lpit0_ch0_flag_counter=0;
+int pushup=0;
+int level=0;
 
 
 
@@ -32,17 +34,93 @@ int main(void) {
 
  for (;;) {
 
-	 	 if (PUSHUP)
+	 if ( PUSHUP)
 	 	 {
-	 		PTD-> PCOR |= 1<<PTD0; /* Clear Output on */
-	 	 }
-	 	 else
-	 	 {
-	 		PTD-> PSOR |= 1<<PTD0; /* Set Output off */
-	 	 }
 
- }
+	 		 pushup=1;
+	 	 }
+	 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	 		 if( pushup == 1 )
+	 		 {
+	 			 level=0;
+
+	 			 			 DisableTimer100ms ();
+	 			 			 lpit0_ch0_flag_counter=0;
+
+	 			 			 PTD-> PCOR |= 1<<PTD0; /* Clear Output on port D0 (LED on) */
+
+	 			 			  EnableTimer400ms ();
+
+	 			 			  while (lpit0_ch0_flag_counter<1){}
+
+	 			 			  DisableTimer400ms ();
+	 			 			  lpit0_ch0_flag_counter=0;
+	 			 			  level++;
+
+	 			 			  if (level==1)
+	 			 			  {
+
+	 			 			  PTE-> PCOR |= 1<<PTE1; /*Clear Output on port E1 (LED on) */
+
+	 			 			 	 	 	 EnableTimer400ms ();
+
+	 			 			 			  while (lpit0_ch0_flag_counter<1){}
+
+	 			 			 			  DisableTimer400ms ();
+	 			 			 			  lpit0_ch0_flag_counter=0;
+	 			 			 			  level++;
+	 			 			  }
+
+	 			 			  if(level==2)
+	 			 			  {
+
+	 			 			 PTC-> PCOR |= 1<<PTC14; /* Clear output on port C14 (POT)  (LED on) */
+
+
+	 			 			 	 	 	 	 	      EnableTimer400ms ();
+
+	 			 			 			 			  while (lpit0_ch0_flag_counter<1){}
+
+	 			 			 			 			  DisableTimer400ms ();
+	 			 			 			 			  lpit0_ch0_flag_counter=0;
+	 			 			  }
+
+
+
+	 		 }
+
+
+	 			 	 	 	 	PTD-> PSOR |= 1<<PTD0; /* Set Output on port D0 (LED off) */
+	 			 		 		PTD-> PSOR |= 1<<PTD16; /* Set Output on port D16 (LED off) */
+	 			 		 		PTC-> PSOR |= 1<<PTC3; /* Set Output on port C3 (LED off) */
+	 			 		 		PTC-> PSOR |= 1<<PTC14; /* Set Output on port C14 (LED off) */
+	 			 		 		PTB-> PSOR |= 1<<PTB16; /* Set Output on port B16 (LED off) */
+	 			 		 		PTB-> PSOR |= 1<<PTB15; /* Set Output on port B15 (LED off) */
+	 			 		 		PTB-> PSOR |= 1<<PTB14; /* Set Output on port B14 (LED off) */
+	 			 		 		PTB-> PSOR |= 1<<PTB17; /* Set Output on port B17 (LED off) */
+	 			 		 		PTC-> PSOR |= 1<<PTC6; /* Set Output on port C6 (LED off) */
+	 			 		 		PTC-> PSOR |= 1<<PTC7; /* Set Output on port C7 (LED off) */
+	 			 		 		PTE-> PSOR |= 1<<PTE16; /* Set Output on port A4 (LED off) */
+	 			 		 		PTE-> PSOR |= 1<<PTE15; /* Set Output on port A10 (LED off) */
+	 			 		 		PTE-> PSOR |= 1<<PTE14; /* Set Output on port E14 (LED off) */
+	 			 		 		PTE-> PSOR |= 1<<PTE13; /* Set Output on port E13 (LED off) */
+	 			 		 		PTE-> PSOR |= 1<<PTE1; /*Set Output on port E1 (LED off) */
+
+	 			 		 		EnableTimer400ms ();
+
+	 			 		 					 			 			 			  while (lpit0_ch0_flag_counter<1){}
+
+	 			 		 					 			 			 			  DisableTimer400ms ();
+	 			 		 					 			 			 			  lpit0_ch0_flag_counter=0;
+
+
+
+	 }
 }
+
+
+
 
 void LPIT0_Ch0_IRQHandler (void) {
  lpit0_ch0_flag_counter++; /* Increment LPIT0 timeout counter */
