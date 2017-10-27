3ffd21740dac4a0918203cce8fd7acc2ebe41cf8
MemoHernandez20 <33108716+MemoHernandez20@users.noreply.github.com>
2017-10-27 12:41:49 -0500

    Code_Version_6
    Create a new evaluation for buttons.
    "One-Touch and Manual" Mode Availables for Up & Down Behavior
    Create function-Routines for the simulation of the window (switch-case).
    Indicators of the Behavior Mode (UP = BLUE; DOWN=GREEN)
    AntiPinch Mode available on the "One-Touch" Up mode. (Anti Pinch with a RED LED indicator)

--- Window_Lifter_Code/WindowLifter/src/Mode_Routine.c
+++ Window_Lifter_Code/WindowLifter/src/Mode_Routine.c
@@ -1,99 +1,276 @@
 #include "S32K144.h" /* include peripheral declarations S32K144 */
-#include "Mode_Routine.h"
 #include "initproyect.h"
+#include "clocks_and_modes.h"
+#include "time100ms.h"
+#include "time400ms.h"
+#include "Mode_Routine.h"
 
 
-void Down_Mode_Routine (int *window_level)
 
-{
-switch ( (*window_level) )
+int lpit0_ch0_flag_counter = 0; /* LPIT0 chan 0 timeout counter */
+int lpit0_ch1_flag_counter = 0; /*LPIT0 chan1 timeout counter*/
+int one_touch_mode_flag_up=0;
+int manual_mode_flag_up=0;
+int level=0;
+int pushup=0;
+int pushdown=0;
+int push_button_pressed=0;
+int pushup1 =0;
+int counter=0;
+int flag_down=0;
 
-{
 
-case 0: //PTD-> PSOR |= 1<<PTD0; /* Clear Output on port D0 (LED on) */
-break;
-case 1: PTE-> PSOR |= 1<<PTE1; /* Clear Output on port C7 (LED on) */
-break;
-case 2: PTC-> PSOR |= 1<<PTC14; /* Clear Output on port B17 (LED on) */
-break;
-case 3: PTE-> PSOR |= 1<<PTE14; /* Clear Output on port B14 (LED on) */
-break;
-case 4: PTE-> PSOR |= 1<<PTE15; /* Clear Output on port B15 (LED on) */
-break;
-case 5: PTE-> PSOR |= 1<<PTE16; /* Clear Output on port B16 (LED on) */
-break;
-case 6: PTB-> PSOR |= 1<<PTB16; /* Clear Output on port E16 (LED on) */
-break;
-case 7: PTB-> PSOR |= 1<<PTB15; /* Clear Output on port E15 (LED on) */
-break;
-case 8: PTB-> PSOR |= 1<<PTB14; /* Clear Output on port E14 (LED on) */
-break;
-case 9: PTB-> PSOR |= 1<<PTB17; /* Clear output on port C14 (POT)  (LED on) */
-break;
-case 10: PTC-> PSOR |= 1<<PTC7; /*Clear Output on port E1 (LED on) */
-break;
-default: //*window_level=0;
-break;
 
-}
- 
+
+void WDOG_disable (void){
+ WDOG->CNT=0xD928C520; /*Unlock watchdog*/
+ WDOG->TOVAL=0x0000FFFF; /*Maximum timeout value*/
+ WDOG->CS = 0x00002100; /*Disable watchdog*/
 }
 
-void Up_Mode_Routine (int *window_level)
+int main(void) {
+ WDOG_disable();
+ SOSC_init_8MHz(); /* Initialize system oscillator for 8 MHz xtal */
+ SPLL_init_160MHz(); /* Initialize SPLL to 160 MHz with 8 MHz SOSC */
+ NormalRUNmode_80MHz(); /* Init clocks: 80 MHz sysclk & core, 40 MHz bus, 20 MHz flash */
+ NVIC_init_IRQs(); /* Enable desired interrupts and priorities */
+ LPIT0_init(); /* Initialize PIT0*/
+ EnablePCC ();
+ WindowInit ();
+ IndicatorsInit();
+ ButtonsInit ();
+
+
+
+ 		 	 	 	 	 	 	 	 	 	 	 	 	PTD-> PSOR |= 1<<PTD0; /* Set Output on port D0 (LED off) */
+ 		 	 				 			 		 		PTD-> PSOR |= 1<<PTD16; /* Set Output on port D16 (LED off) */
+ 		 	 				 			 		 		PTC-> PSOR |= 1<<PTC3; /* Set Output on port C3 (LED off) */
+ 		 	 				 			 		 		PTC-> PSOR |= 1<<PTC14; /* Set Output on port C14 (LED off) */
+ 		 	 				 			 		 		PTB-> PSOR |= 1<<PTB16; /* Set Output on port B16 (LED off) */
+ 		 	 				 			 		 		PTB-> PSOR |= 1<<PTB15; /* Set Output on port B15 (LED off) */
+ 		 	 				 			 		 		PTB-> PSOR |= 1<<PTB14; /* Set Output on port B14 (LED off) */
+ 		 	 				 			 		 		PTB-> PSOR |= 1<<PTB17; /* Set Output on port B17 (LED off) */
+ 		 	 				 			 		 		PTC-> PSOR |= 1<<PTC6; /* Set Output on port C6 (LED off) */
+ 		 	 				 			 		 		PTC-> PSOR |= 1<<PTC7; /* Set Output on port C7 (LED off) */
+ 		 	 				 			 		 		PTE-> PSOR |= 1<<PTE16; /* Set Output on port A4 (LED off) */
+ 		 	 				 			 		 		PTE-> PSOR |= 1<<PTE15; /* Set Output on port A10 (LED off) */
+ 		 	 				 			 		 		PTE-> PSOR |= 1<<PTE14; /* Set Output on port E14 (LED off) */
+ 		 	 				 			 		 		PTE-> PSOR |= 1<<PTE13; /* Set Output on port E13 (LED off) */
+ 		 	 				 			 		 		PTE-> PSOR |= 1<<PTE1; /*Set Output on port E1 (LED off) */
+ 		 	 				 			 		 		PTD-> PSOR |= 1<<PTD15; /* Clear Output on port D0 (LED on) */
+
+
+
+
+ for (;;) {
+
+while ( ( pushup == 0 ) && ( pushdown == 0 )  )
 {
-	switch ( (*window_level) ) {
-					 case 0:
-						 			 			 //PTD-> PCOR |= 1<<PTD0; /* Clear Output on port D0 (LED on) */
+	 if (PUSHUP)
+	 {
+		 EnableTimer100ms();
 
-						 break;
-					 case 1:
-			 	 	 	 	 	 	 	 	 	 PTE-> PCOR |= 1<<PTE1; /*Clear Output on port E1 (LED on) */
+		 while ( ( (PUSHUP) && (lpit0_ch0_flag_counter<5)) ) {} /*Do Nothing*/
+		 	 	 DisableTimer100ms ();
+		 		 if(lpit0_ch0_flag_counter>=5)
+		 		 {
+		 		 pushup=2; /*MANUAL_MODE_FLAG*/
+		 		 }
+		 		 else if( (lpit0_ch0_flag_counter<5) && (lpit0_ch0_flag_counter != 0) )
+		 		 {
+		 			 pushup=1; /*ONE_TOUCH_MODE_FLAG*/
+		 		 }
 
-						 break;
-					 case 2:
-						 	 	 	 	 	 	 PTC-> PCOR |= 1<<PTC14; /* Clear output on port C14 (POT)  (LED on) */
-
-						 break;
-					 case 3:
-						 	 	 	 	 	 	 PTE-> PCOR |= 1<<PTE14; /* Clear Output on port E14 (LED on) */
-
-						 break;
-					 case 4:
-						 	 	 	 	 	 	 PTE-> PCOR |= 1<<PTE15; /* Clear Output on port E15 (LED on) */
-
-						 break;
-					 case 5:
-						 	 	 	 	 	 	 PTE-> PCOR |= 1<<PTE16; /* Clear Output on port E16 (LED on) */
-
-						 break;
-					 case 6:
-						 	 	 	 	 	 	 PTB-> PCOR |= 1<<PTB16; /* Clear Output on port B16 (LED on) */
-
-						 break;
-					 case 7:
-						 	 	 	 	 	 	 PTB-> PCOR |= 1<<PTB15; /* Clear Output on port B15 (LED on) */
-
-						 break;
-					 case 8:
-						 	 	 	 	 	 	 PTB-> PCOR |= 1<<PTB14; /* Clear Output on port B14 (LED on) */
+	}
 
 
-						 break;
-					 case 9:
-						 	 	 	 	 	 	 PTB-> PCOR |= 1<<PTB17; /* Clear Output on port B17 (LED on) */
+	 if (PUSHDOWN)
+	 		 {
 
+	 			 EnableTimer100ms();
 
-						 break;
-					 case 10:
-						 	 	 	 	 	 	 PTC-> PCOR |= 1<<PTC7; /* Clear Output on port C7 (LED on) */
+	 			 		 while ( ( (PUSHDOWN) && (lpit0_ch0_flag_counter<5)) ) {} /*Do Nothing*/
+	 			 		 	 	 DisableTimer100ms ();
+	 			 		 		 if(lpit0_ch0_flag_counter>=5)
+	 			 		 		 {
+	 			 		 		 pushdown=2; /*MANUAL_MODE_FLAG*/
+	 			 		 		 }
+	 			 		 		 else if( (lpit0_ch0_flag_counter<5) && (lpit0_ch0_flag_counter != 0) )
+	 			 		 		 {
+	 			 		 			 pushdown=1; /*ONE_TOUCH_MODE_FLAG*/
+	 			 		 		 }
+	 		 }
 
-
-			  	  	  	  	  	  	  	  	  	  	  	  	 // *window_level=0;
-						 break;
-
-					 default:
-						 	 	 	 	 	 	 *window_level=10;
-						 break;
-
-					 }
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	 if(pushup==1)
+	 {
+		 while(level<11 && pushup==1)
+		 {
+			 PTD-> PCOR |= 1<<PTD0; /* Clear Output on port D0 (LED on) */
+			 DisableTimer100ms();
+			 lpit0_ch0_flag_counter=0;
+			 	 	 	 	 	 Up_Mode_Routine (&level);
+		 		 		 		 EnableTimer400ms();
+		 		 		 		 while (lpit0_ch0_flag_counter<1){}
+		 		 		 		 DisableTimer400ms();
+		 		 		 		 lpit0_ch0_flag_counter=0;
+		 		 		 		 level++;
+		 	if(ANTIPINCH)
+		 	{
+		 		DisableTimer100ms();
+		 		lpit0_ch0_flag_counter=0;
+		 		EnableTimer100ms();
+
+		 		while ( ( (ANTIPINCH) && (lpit0_ch0_flag_counter<1)) ) {} /*Do Nothing*/
+		 		DisableTimer100ms ();
+		 		if(ANTIPINCH)
+		 		{
+		 			while(level>=1)
+		 					 				 		 {
+		 												 PTD-> PSOR |= 1<<PTD0; /* Clear Output on port D0 (LED on) */
+		 												 PTD-> PCOR |= 1<<PTD15; /* Clear Output on port D16 (LED on) */
+		 					 				 			 DisableTimer100ms();
+		 					 				 			 lpit0_ch0_flag_counter=0;
+		 					 				 			 	 	 	 	 	 Down_Mode_Routine (&level);
+		 					 				 		 		 		 		 EnableTimer400ms();
+		 					 				 		 		 		 		 while (lpit0_ch0_flag_counter<1){}
+		 					 				 		 		 		 		 DisableTimer400ms();
+		 					 				 		 		 		 		 lpit0_ch0_flag_counter=0;
+		 					 				 		 		 		 		 level--;
+		 					 				 		 }
+
+		 					 				 		 if(level==0)
+		 					 				 		 {
+		 					 				 			 pushdown=0;
+		 					 				 			 level=0;
+		 					 				 			PTD-> PSOR |= 1<<PTD15; /* Clear Output on port D16 (LED on) */
+		 					 				 			pushup=0;
+
+		 					 				 		 }
+		 			DisableTimer100ms();
+		 			lpit0_ch0_flag_counter=0;
+		 			EnableTimer100ms();
+		 			while (lpit0_ch0_flag_counter<50)
+		 			{
+		 				/*Do nothing*/
+		 			}
+		 			pushup=0;
+		 			pushdown=0;
+		 			DisableTimer100ms();
+		 			lpit0_ch0_flag_counter=0;
+		 			level=0;
+		 		}
+		 	}
+		 }
+
+		 if(level>=11)
+		 {
+			 level=10;
+			 pushup=0;
+			 PTD-> PSOR |= 1<<PTD0; /* Clear Output on port D16 (LED on) */
+		 }
+	 }
+/////////////////////                                     //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////                                    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	 else if(pushup==2)
+	 {
+		 DisableTimer100ms();
+		 lpit0_ch0_flag_counter=0;
+		 while(PUSHUP)
+		 		 {
+			 	 PTD-> PCOR |= 1<<PTD0; /* Clear Output on port D0 (LED on) */
+		 		 Up_Mode_Routine (&level);
+		 		 		 		 		 EnableTimer400ms();
+		 		 		 		 		 while (lpit0_ch0_flag_counter<1){}
+		 		 		 		 		 DisableTimer400ms();
+		 		 		 		 		 lpit0_ch0_flag_counter=0;
+		 		 		 		 		 level++;
+		 		 }
+		 PTD-> PSOR |= 1<<PTD0; /* Clear Output on port D16 (LED on) */
+		 if(level>=10)
+		 {
+			 level=10;
+			 pushup=0;
+		 }
+		 else
+		 {
+			pushup=0;
+		 }
+	 }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	 else if (pushdown==1)
+	 {
+
+		 				 while(level>=1)
+		 				 		 {
+		 					 	 	 PTD-> PCOR |= 1<<PTD16; /* Clear Output on port D16 (LED on) */
+		 				 			 DisableTimer100ms();
+		 				 			 lpit0_ch0_flag_counter=0;
+		 				 			 	 	 	 	 	 Down_Mode_Routine (&level);
+		 				 		 		 		 		 EnableTimer400ms();
+		 				 		 		 		 		 while (lpit0_ch0_flag_counter<1){}
+		 				 		 		 		 		 DisableTimer400ms();
+		 				 		 		 		 		 lpit0_ch0_flag_counter=0;
+		 				 		 		 		 		 level--;
+		 				 		 }
+
+		 				 		 if(level==0)
+		 				 		 {
+		 				 			 pushdown=0;
+		 				 			 level=0;
+		 				 			PTD-> PSOR |= 1<<PTD16; /* Clear Output on port D16 (LED on) */
+
+		 				 		 }
+
+	 }
+
+	 else if (pushdown==2)
+	 {
+		 	 	 	 	 while(PUSHDOWN)
+		 		 				 		 {
+		 	 	 	 		 	 	 	 	 PTD-> PCOR |= 1<<PTD16; /* Clear Output on port D16 (LED on) */
+		 	 	 	 		 	 	 	 	 if(level !=0)
+		 	 	 	 		 	 	 	 	 {
+		 		 				 			 DisableTimer100ms();
+		 		 				 			 lpit0_ch0_flag_counter=0;
+		 		 				 			 	 	 	 	 	 Down_Mode_Routine (&level);
+		 		 				 		 		 		 		 EnableTimer400ms();
+		 		 				 		 		 		 		 while (lpit0_ch0_flag_counter<1){}
+		 		 				 		 		 		 		 DisableTimer400ms();
+		 		 				 		 		 		 		 lpit0_ch0_flag_counter=0;
+		 		 				 		 		 		 		 level--;
+		 	 	 	 		 	 	 	 	 }
+		 		 				 		 }
+		 	 	 	 	 	 	 	 	 PTD-> PSOR |= 1<<PTD16; /* Clear Output on port D16 (LED on) */
+
+		 		 				 		 if(level==0)
+		 		 				 		 {
+		 		 				 			 pushdown=0;
+		 		 				 			 level=0;
+		 		 				 			 pushup=0;
+
+		 		 				 		 }
+		 		 				 		 else
+		 		 				 		 {
+		 		 				 			 pushdown=0;
+		 		 				 		 }
+	 }
+
+}
+
+
+}
+
+void LPIT0_Ch0_IRQHandler (void) {
+ lpit0_ch0_flag_counter++; /* Increment LPIT0 timeout counter */
+ counter++;
+ //PTD->PTOR |= 1<<0; /* Toggle output on port D0 (blue LED) */
+ //PTC->PTOR |= 1<<7; //C7
+ LPIT0->MSR |= LPIT_MSR_TIF0_MASK; /* Clear LPIT0 timer flag 0 */
+}
+
